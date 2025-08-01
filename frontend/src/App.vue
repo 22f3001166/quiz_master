75155a7d4de7d579a3421860650b1cbad8890f87
Dalyodh Singh <136873615+22f3001166@users.noreply.github.com>
2025-08-01 12:29:57 +0530

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return { 
+      isAuthenticated: this.$store.getters.isAuthenticated,
+      userRole: this.$store.getters.userRole
+    };
+   },
+};
+</script>
+
+<style>
+#app {
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  scroll-behavior: auto;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #0c0c0c;
+}
+
+
+.homeview{
+  width: 100;
+}
+
+
+</style>
+
+ 
